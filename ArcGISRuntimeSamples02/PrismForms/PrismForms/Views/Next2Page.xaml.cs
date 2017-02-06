5884d5d79c570af58cd0efd5830051fa02888f96
kamiya takahiro <kamiya@kamiya-no-MacBook-Air.local>
2017-02-06 19:52:08 +0900

    地図機能の変更

--- ArcGISRuntimeSamples02/PrismForms/PrismForms/Views/Next2Page.xaml.cs
+++ ArcGISRuntimeSamples02/PrismForms/PrismForms/Views/Next2Page.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Esri.ArcGISRuntime.Mapping;
+﻿using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
 using System;
 using Xamarin.Forms;
 
@@ -7,13 +9,21 @@ namespace PrismForms.Views
 	public partial class Next2Page : ContentPage
 	{
 
-		// String array to store titles for the viewpoints specified above.
-		private string[] titles = new string[]
+		private string _navigationUrl = "https://www.arcgis.com/home/item.html?id=dcbbba0edf094eaa81af19298b9c6247";
+		private string _streetUrl = "https://www.arcgis.com/home/item.html?id=4e1133c28ac04cca97693cf336cd49ad";
+		private string _nightUrl = "https://www.arcgis.com/home/item.html?id=bf79e422e9454565ae0cbe9553cf6471";
+		private string _darkGrayUrl = "https://www.arcgis.com/home/item.html?id=850db44b9eb845d3bd42b19e8aa7a024";
+
+		private string _vectorTiledLayerUrl;
+		private ArcGISVectorTiledLayer _vectorTiledLayer;
+
+		// String array to store some vector layer choices
+		private string[] _vectorLayerNames = new string[]
 		{
-			"Topo",
+			"Dark gray",
 			"Streets",
-			"Imagery",
-			"Ocean"
+			"Night",
+			"Navigation"
 		};
 
 		// String array to store titles for the viewpoints specified above.
@@ -21,58 +31,87 @@ namespace PrismForms.Views
 		{
 			InitializeComponent();
 
-			initBasemap();
+			initVectorTiledLayer();
 		}
 
-		private async void OnChangeBasemapButtonClicked(object sender, EventArgs e)
+		private async void OnChangeLayerButtonClicked(object sender, EventArgs e)
 		{
 			// Show sheet and get title from the selection
-			var selectedBasemap =
-				await DisplayActionSheet("Select basemap", "Cancel", null, titles);
+			var selectedLayer =
+				await DisplayActionSheet("Select basemap", "Cancel", null, _vectorLayerNames);
 
 			// If selected cancel do nothing
-			if (selectedBasemap == "Cancel") return;
+			if (selectedLayer == "Cancel") return;
 
-			switch (selectedBasemap)
+			switch (selectedLayer)
 			{
-				case "Topo":
-
-					// Set the basemap to Topographic
-					MyMapView.Map.Basemap = Basemap.CreateTopographic();
+				case "Dark gray":
+					_vectorTiledLayerUrl = _darkGrayUrl;
 					break;
 
 				case "Streets":
-
-					// Set the basemap to Streets
-					MyMapView.Map.Basemap = Basemap.CreateStreets();
+					_vectorTiledLayerUrl = _streetUrl;
 					break;
 
-				case "Imagery":
-
-					// Set the basemap to Imagery
-					MyMapView.Map.Basemap = Basemap.CreateImagery();
+				case "Night":
+					_vectorTiledLayerUrl = _nightUrl;
 					break;
 
-				case "Ocean":
-
-					// Set the basemap to Imagery
-					MyMapView.Map.Basemap = Basemap.CreateOceans();
+				case "Navigation":
+					_vectorTiledLayerUrl = _navigationUrl;
 					break;
 
 				default:
 					break;
 			}
+
+			// Create a new ArcGISVectorTiledLayer with the Url Selected by the user
+			_vectorTiledLayer = new ArcGISVectorTiledLayer(new Uri(_vectorTiledLayerUrl));
+
+			// // Create new Map with basemap and assigning to the Mapviews Map
+			MyMapView.Map = new Map(new Basemap(_vectorTiledLayer));
+
+			addFeatureLayer();
+
 		}
 
-		private void initBasemap()
+		private void initVectorTiledLayer()
 		{
-			// Create new Map with basemap
-			Map myMap = new Map(Basemap.CreateTopographic());
+			// Create a new ArcGISVectorTiledLayer with the navigation serice Url
+			_vectorTiledLayer = new ArcGISVectorTiledLayer(new Uri(_nightUrl));
 
-			// Assign the map to the MapView
-			MyMapView.Map = myMap;
+			// Create new Map with basemap
+			MyMapView.Map = new Map(new Basemap(_vectorTiledLayer));
+
+			addFeatureLayer();
+
 		}
 
+		private void addFeatureLayer()
+		{
+
+			// Create and set initial map area
+			Envelope initialLocation = new Envelope(
+				139.6297352, 35.6006059000001, 139.7937877, 35.7308676000001,
+				SpatialReferences.Wgs84);
+			MyMapView.Map.InitialViewpoint = new Viewpoint(initialLocation);
+
+			// Create uri to the used feature service
+			var serviceUri = new Uri("https://services5.arcgis.com/HzGpeRqGvs5TMkVr/arcgis/rest/services/osmdata/FeatureServer/0");
+
+			// Create feature table for the pools feature service
+			ServiceFeatureTable poolsFeatureTable = new ServiceFeatureTable(serviceUri);
+
+			// Define the request mode
+			poolsFeatureTable.FeatureRequestMode = FeatureRequestMode.OnInteractionCache;
+
+			// Create FeatureLayer that uses the created table
+			FeatureLayer osmLayer = new FeatureLayer(poolsFeatureTable);
+
+			// Add created layer to the map
+			MyMapView.Map.OperationalLayers.Add(osmLayer);
+
+		}
 	}
 
 }
