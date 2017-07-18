587edfda6a3a12da6d597f9a8ebd9f4df5603c50
kamiya <only@jp.bigplanet.com>
2017-07-18 17:14:07 +0900

    add ArcGISRuntimeSamples03

---
+++ ArcGISRuntimeSamples03/ArcGISRuntimeSamples03/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace ArcGISRuntimeSamples03
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ArcGISRuntimeSamples03Page();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ArcGISRuntimeSamples03/ArcGISRuntimeSamples03/ArcGISRuntimeSamples03Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ArcGISRuntimeSamples03
+{
+    public partial class ArcGISRuntimeSamples03Page : ContentPage
+    {
+        public ArcGISRuntimeSamples03Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
